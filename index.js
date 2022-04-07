fbe17a9ff0b1e0490e58c44a6aa834d2a12df0c2
William Hagan <williamhagan1728@yahoo.com>
2022-04-07 15:40:43 -0700

    add endpoint

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/products" , (req, res)=> {
+    console.log(req.headers)
+  res.send("This is a response!");
+})
+
 const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
